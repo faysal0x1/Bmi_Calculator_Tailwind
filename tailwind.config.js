64e49be2e13b6730d84fadf4d8a2b8d718e0eef3
MD. Faysal Rahman <faysal35-3119@diu.edu.bd>
2023-05-08 17:01:49 +0600

    FInal__Project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,37 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 module.exports = {
-    content: ['./*.html'],
-    darkMode: 'class', // or 'media' or 'class'
-    theme: {
-        screens: {
-            sm: '480px',
-            md: '768px',
-            lg: '976px',
-            xl: '1440px'
-        },
-        extend: {
-            fontFamily: {
-                sans: ['Josefin Sans', 'sans-serif'],
-                alata: ['Alata']
-            }
-            ,
-            colors: {
-                //add your own color
-                //https://tailwindcss.com/docs/customizing-colors
-            },
-            letterSpacing: {
-                widest: '.3em'
-            }
-            ,
-            container: {
-                center: true,
-            },
-
-        },
+  content: ["./*.html"],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
     },
-    variants: {
-        extend: {},
+    extend: {
+      backgroundImage: {
+        "bmi":
+          "url('/git_Img/bg_Image.jpg')",
+      },
+      fontFamily: {
+        sans: ["Josefin Sans", "sans-serif"],
+        alata: ["Alata"],
+      },
+      colors: {
+        //add your own color
+        //https://tailwindcss.com/docs/customizing-colors
+      },
+      letterSpacing: {
+        widest: ".3em",
+      },
+      container: {
+        center: true,
+      },
     },
-    plugins: [],
-}
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
